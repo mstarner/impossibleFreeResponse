85b2a124fe67b3a34196608563a2417c51fa6e2c
Matthew Starner <mstarner8@gmail.com>
2017-11-03 08:50:12 -0400

    Update Main.java
    I kind of gave up. Don't know what to do.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,10 +1,14 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 
-
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please type in a word.");
+        int answer = input.nextInt();
 
 
     }
